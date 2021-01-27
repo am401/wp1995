9a749487265116c5698a692d635d9b1568d79b07
am401 <andras.marton@wpengine.com>
2021-01-26 22:29:35 -0600

    Initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="site-footer">
+    <p><?php bloginfo('name') ?></p>
+</footer>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+ 
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post(); ?>
+ 
+    <article class="post">
+        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        <h4>Posted on <?php the_time('F jS, Y') ?></h4>
+        <?php the_excerpt() ?>
+    </article>	
+	<?php endwhile;
+ 
+else :
+	echo '<p>There are no posts!</p>';
+ 
+endif;
+
+posts_nav_link();
+ 
+get_footer();
+?>
